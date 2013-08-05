4582f9830fc1fd9857c57e338cbce4fd67a5bc97
Breyten Ernsting <bje@dds.nl>
2013-08-05 12:08:41 +0200

    Correct format and cleanup.

--- json_to_csv.py
+++ json_to_csv.py
@@ -32,26 +32,26 @@ def load_data():
 def result_to_record(result):
     data = [
         result[u'id'],
-        result[u'author'] or u'',
-        result[u'author_email'] or u'',
-        result[u'license_id'] or u'',
-        result[u'license_title'] or u'',
-        result[u'maintainer'] or u'',
-        result[u'maintainer_email'] or u'',
+        result[u'author'],
+        result[u'author_email'],
+        result[u'license_id'] ,
+        result[u'license_title'],
+        result[u'maintainer'],
+        result[u'maintainer_email'],
         result[u'metadata_created'],
         result[u'metadata_modified'],
         result[u'name'],
         result[u'title'],
         result[u'notes'],
-        result[u'state'] or u'',
+        result[u'state'],
         u','.join([r[u'display_name'] for r in result[u'tags']]),
-        result[u'url'] or u''
+        result[u'url']
     ]
     return data
 
 def tsv_escape(text):
     if text is not None:
-        return text.replace('\n', ' ').replace('\t', ' ').replace('\r', '')
+        return u'"' + text.replace('\n', ' ').replace('\t', ' ').replace('\r', '').replace(u'"', u'\\"') + u'"'
     else:
         return u''
 
@@ -61,7 +61,7 @@ def main():
     #pprint(flat_results)
     with codecs.open('data-overheid.csv', 'w', 'utf-8') as out_file:
         for flat_result in flat_results:
-            out_file.write(u'\t'.join([tsv_escape(f) for f in flat_result]) + u"\n")
+            out_file.write(u','.join([tsv_escape(f) for f in flat_result]) + u"\n")
 
 if __name__ == '__main__':
     main()
